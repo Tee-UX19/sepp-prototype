43b0ac68f7c6d3622d731a180efb29499296faad
fredplewes <fplewes@gmail.com>
2024-11-18 13:18:21 +0000

    started implementing bootstrap

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import './CardList.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CardList = () => {
   const members = [
-    { name: 'User', amountOwed: '$15', paid: '$10', profilePic: 'https://placehold.co/150'},
-    { name: 'User', amountOwed: '$15', paid: '$10', profilePic: 'https://placehold.co/150'},
-    { name: 'User', amountOwed: '$15', paid: '$10', profilePic: 'https://placehold.co/150'},
-    { name: 'User', amountOwed: '$15', paid: '$10', profilePic: 'https://placehold.co/150'},
+    { name: 'User (YOU)', amountOwed: '$15', paid: '$10', profilePic: 'https://placehold.co/150'},
     { name: 'User', amountOwed: '$15', paid: '$10', profilePic: 'https://placehold.co/150'},
     { name: 'User', amountOwed: '$15', paid: '$10', profilePic: 'https://placehold.co/150'},
     { name: 'User', amountOwed: '$15', paid: '$10', profilePic: 'https://placehold.co/150'},
@@ -14,15 +11,27 @@ const CardList = () => {
   ];
 
   return (
-    <div className="card-list">
+    <div className="container" style={{ minHeight: '400px', minWidth:'75%'}}>
+      <div className="row flex-nowrap overflow-auto">
       {members.map((member, index) => (
-        <div className="card" key={index}>
-          <h2>{member.name}</h2>
-          <img className="profile-pic" src={member.profilePic} alt={`${member.name}'s profile`} />
-          <h3>Amount Owed: {member.amountOwed}</h3>
-          <h3>Paid: {member.paid}</h3>
+        <div key={index} className="col-10 col-md-4 col-lg-4">
+        <div className="card mb-4" style={{ minHeight: '400px' }}>
+          <div className="card-body">
+            <h2 className="card-title">{member.name}</h2>
+            <img src={member.profilePic} alt={`${member.name}'s profile`} className="rounded-circle mx-auto d-block p-2" 
+            style={{ width: '150px', height: '150px', objectFit: 'cover'}}
+          />
+            <p className="card-text">
+              <strong>Amount Owed:</strong> {member.amountOwed}
+            </p>
+            <p className="card-text">
+              <strong>Paid:</strong> {member.paid}
+            </p>
+          </div>
         </div>
+      </div>
       ))}
+      </div>
     </div>
   );
 };
--- src/components/CheckoutPage.jsx
+++ src/components/CheckoutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardList from './CardList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CheckoutPage = () => {
   return (
